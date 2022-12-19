7ac099c6907228c5008157a0eb49bbde0e5d9315
Abdallah El belkasy <abdallahemohmad2002@gmail.com>
2022-12-19 18:07:44 +0200

    [Final] Added Report Automation

--- Decision_Trees_on_Banknote_Authentication_Data.py
+++ Decision_Trees_on_Banknote_Authentication_Data.py
@@ -1,5 +1,8 @@
 # In [0]:
 import random
+from datetime import datetime
+
+import fpdf
 import graphviz
 import numpy as np
 import pandas as pd
@@ -9,6 +12,19 @@ from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
 
+pdf = fpdf.FPDF()
+
+pdf.add_page()
+
+pdf.set_font("Arial", 'B', size=32)
+pdf.cell(200, 10, txt="Final Report", ln=2, align='C')
+currentDateAndTime = datetime.now()
+pdf.set_font("Arial", size=16)
+pdf.cell(200, 10, txt=str(currentDateAndTime), ln=8, align='C')
+
+pdf.add_page()
+pdf.set_font('arial', '', 10)
+ln = 1
 # In[1]:
 # split data to test and train data
 dataInput = pd.read_csv("BankNote_Authentication.csv")
@@ -17,6 +33,7 @@ y = dataInput['class']
 
 
 def train(train_size):
+    global ln
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size)
 
     clf = tree.DecisionTreeClassifier()
@@ -27,11 +44,17 @@ def train(train_size):
                                special_characters=True, feature_names=X.columns.values, class_names=['0', '1'])
 
     graph = graphviz.Source(dot_data)
-    graph.render(f'graphs/BankNote_Authentication_tree_for_{train_size * 100}%')
+
+    imageFile = f'./graphs/BankNote_Authentication_tree_for_Size_{train_size * 100}%'
+    graph.render(imageFile, format='png')
 
     y_pred = clf.predict(X_test)
     s = train_size * 100
     acc = metrics.accuracy_score(y_test, y_pred) * 100
+    pdf.cell(0, 10, txt=f"For Split Size {s}%\tThe Accuracy: {acc}%", ln=ln)
+    ln += 1
+    pdf.cell(0, 10, txt="Check to See the Image in \"" + imageFile + '.png\"', ln=ln)
+    ln += 2
 
     print(f"For Split Size {s}%\tThe Accuracy: {acc}%")
 
@@ -39,7 +62,6 @@ def train(train_size):
 # for 25%
 train(0.25)
 print("=========" * 20)
-
 # Five Random Sizes
 for i in range(5):
     train(round(random.random(), 4))
@@ -51,6 +73,7 @@ set_size = [.3, .4, .5, .6, .7]
 
 
 def train_five_times(train_size):
+    global ln
     Accuracy = np.zeros(5)
     tree_size = np.zeros(5)
     for num in range(5):
@@ -66,7 +89,9 @@ def train_five_times(train_size):
                                    special_characters=True, feature_names=X.columns.values, class_names=['0', '1'])
 
         graph = graphviz.Source(dot_data)
-        graph.render(f'graphs/BankNote_Authentication_tree_for_{train_size * 100}%')
+
+        imageFile = f'./graphs/BankNote_Authentication_tree_for_Size_{train_size * 100}%'
+        graph.render(imageFile, format='png')
 
         y_pred = clf.predict(X_test)
         acc = metrics.accuracy_score(y_test, y_pred)
@@ -74,7 +99,15 @@ def train_five_times(train_size):
         Accuracy[num] = acc
         tree_size[num] = clf.tree_.node_count
 
-        print(f"In Test number {num}\tFor Split Size{train_size * 100}%\twith Random Seed {rand}\tThe Accuracy: {acc * 100}%")
+        pdf.cell(0, 10,
+                 txt=f"In Test number {num}\tFor Split Size{train_size * 100}%\twith Random Seed {rand}\tThe Accuracy: {acc * 100}%",
+                 ln=ln)
+        ln += 2
+
+        pdf.cell(0, 10, txt="Check to See the Image in \"" + imageFile + '.png\"', ln=ln)
+        ln += 2
+        print(
+            f"In Test number {num}\tFor Split Size{train_size * 100}%\twith Random Seed {rand}\tThe Accuracy: {acc * 100}%")
 
     s = train_size * 100
 
@@ -84,6 +117,18 @@ def train_five_times(train_size):
     print(f'For the Tree sizes \t mean={tree_size.mean()} minimum={min(tree_size)} maximum={max(tree_size)}\n')
     print("=========" * 20)
 
+    pdf.cell(0, 10, txt="=========" * 20, ln=ln)
+    ln += 1
+    pdf.cell(0, 10, txt=
+    f'\nFor Split Set_Size {s}% \t mean={Accuracy.mean()} minimum={min(Accuracy)} maximum={max(Accuracy)}', ln=ln)
+    ln += 1
+    pdf.cell(0, 10,
+             txt=f'For the Tree sizes \t mean={tree_size.mean()} minimum={min(tree_size)} maximum={max(tree_size)}\n',
+             ln=ln)
+    ln += 1
+    pdf.cell(0, 10, txt="=========" * 20, ln=ln)
+    ln += 1
+
     accuracy_history.append([s, Accuracy.mean()])
     return clf.tree_.node_count
 
@@ -91,6 +136,8 @@ def train_five_times(train_size):
 node_count = []
 
 for i in set_size:
+    ln = 1
+    pdf.add_page()
     node_count.append(train_five_times(i))
 
 
@@ -115,4 +162,11 @@ plot_rel(set_size, node_count, "The number of nodes in the final tree against tr
          "Tree Size")
 
 node_count = np.array(node_count)
+
 print(f'For All the Trees mean={node_count.mean()} minimum={min(node_count)} maximum={max(node_count)}')
+
+ln += 5
+pdf.set_font("Arial", size=14)
+pdf.cell(0, 10, txt=f'For All the Trees mean={node_count.mean()} minimum={min(node_count)} maximum={max(node_count)}',
+         ln=ln)
+pdf.output(f"Final_Report.pdf")
